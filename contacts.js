a993e07b21efb1ccfcf17a18bf2dbad579dc10dd
MorskoySom <Somovvitvit@gmail.com>
2023-12-08 18:20:40 +0200

    12

--- contacts.js
+++ contacts.js
@@ -1,21 +1,33 @@
 import fs from "fs/promises";
 import path from "path";
+import { nanoid } from 'nanoid';
 
 const contactsPath = path.resolve("db", "contacts.json");
 
 export const listContacts = async () => {
-    const data = await fs.readFile(contactsPath, "utf-8");
+    const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
 }
 
-export function getContactById(contactId) {
-  // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
+}
+
+export const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  } 
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 }
 
 export function removeContact(contactId) {
   // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
-}
-
-export function addContact(name, email, phone) {
-  // ...твой код. Возвращает объект добавленного контакта. 
 }
--- index.js
+++ index.js
@@ -2,12 +2,21 @@ console.log(`Hello, bro`);
 
 import * as contactSevice from "./contacts.js"
 
-const invokeAction = async ({ action }) => {
+const invokeAction = async ({ action, contactId, name, email, phone }) => {
     switch (action) {
         case "list":
             const allContacts = await contactSevice.listContacts();
             return console.log(allContacts);
+        case "getById":
+            const getingContact = await contactSevice.getContactById(contactId);
+            return console.log(getingContact);
+        case "add":
+            const newContact = await contactSevice.addContact(name, email, phone);
+            return console.log(newContact);
+
     }
 }
 
-invokeAction({ action: "list" });
+// invokeAction({ action: "list" });
+// invokeAction({ action: "getById", contactId: 'AeHIrLTr6JkxGE6SN-0Rw' });
+// invokeAction({ action: "add", name: 'Paha Som', email: 'paha@gmail.com', phone: '(066) 479-3010'});
